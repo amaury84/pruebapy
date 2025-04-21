5888c716eec46e28d8715a03b686393f526d389d
Amaury <amaury84@gmail.com>
2025-04-21 14:19:30 -0500

    agregas pruebas unitarias de suma y resta

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from operaciones import sumar,restar
-
+import pytest
 def saludar(nombre):
     print("hola "+nombre)
 
@@ -9,6 +9,14 @@ def main():
     print(sumar(7,5))
     saludar("Maria")
     print(restar(5,7))
+
+def test_resta():
+    assert restar(10,5)==5
+    assert restar(5,5)==0
+    assert restar(0,5)==-5
+
+def test_suma():
+    assert sumar(7)==10
 
 if __name__=="__main__":
     main()
