1719ab935a92a869d59b32cf10dce9274e50d8c2
Amaury <amaury84@gmail.com>
2025-04-21 14:06:17 -0500

    agregue una resta en app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from operaciones import sumar
+from operaciones import sumar,restar
 
 def saludar(nombre):
     print("hola "+nombre)
@@ -8,6 +8,7 @@ def main():
     print(sumar(7))
     print(sumar(7,5))
     saludar("Maria")
+    print(restar(5,7))
 
 if __name__=="__main__":
     main()
